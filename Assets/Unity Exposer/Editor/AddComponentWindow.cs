ff9b2d67c225a111ee3254a391dbaf4fe33975e0
GlitchEnzo <mccarthp@gmail.com>
2015-05-04 10:47:36 -0700

    Exposed more members
    Exposed more methods in PreviewGUI (should be all of them).
    
    Exposed AddComponentWindow's static methods.

---
+++ Assets/Unity Exposer/Editor/AddComponentWindow.cs
@@ -0,0 +1,68 @@
+﻿namespace UnityEditor.Exposer
+{
+    using UnityEngine;
+    using System;
+    using System.Reflection;
+
+    public class AddComponentWindow
+    {
+        private static Type ActualType { get; set; }
+
+        public static string className
+        {
+            get
+            {
+                PropertyInfo propertyInfo = ActualType.GetProperty("className", BindingFlags.NonPublic | BindingFlags.Static);
+                return (string)propertyInfo.GetGetMethod().Invoke(null, null);
+            }
+            set
+            {
+                PropertyInfo propertyInfo = ActualType.GetProperty("className", BindingFlags.NonPublic | BindingFlags.Static);
+                propertyInfo.GetSetMethod().Invoke(null, new object[] { value });
+            }
+        }
+
+        public static GameObject[] gameObjects
+        {
+            get
+            {
+                PropertyInfo propertyInfo = ActualType.GetProperty("gameObjects", BindingFlags.NonPublic | BindingFlags.Static);
+                return (GameObject[])propertyInfo.GetGetMethod().Invoke(null, null);
+            }
+            set
+            {
+                PropertyInfo propertyInfo = ActualType.GetProperty("gameObjects", BindingFlags.NonPublic | BindingFlags.Static);
+                propertyInfo.GetSetMethod().Invoke(null, new object[] { value });
+            }
+        }
+
+        static AddComponentWindow()
+        {
+            ActualType = Type.GetType("UnityEditor.AddComponentWindow, UnityEditor", true);
+        }
+
+        public static void ExecuteAddComponentMenuItem()
+        {
+            MethodInfo methodInfo = ActualType.GetMethod("ExecuteAddComponentMenuItem", BindingFlags.NonPublic | BindingFlags.Static);
+            methodInfo.Invoke(null, null);
+        }
+
+        public static bool Show(Rect rect, GameObject[] gos)
+        {
+            MethodInfo methodInfo = ActualType.GetMethod("Show", BindingFlags.NonPublic | BindingFlags.Static);
+            return (bool)methodInfo.Invoke(null, new object[] { rect, gos });
+        }
+
+        public static bool ValidateAddComponentMenuItem()
+        {
+            MethodInfo methodInfo = ActualType.GetMethod("ValidateAddComponentMenuItem", BindingFlags.NonPublic | BindingFlags.Static);
+            return (bool)methodInfo.Invoke(null, null);
+        }
+
+        //public void OnGUI()
+        //{
+        //    MethodInfo methodInfo = ActualType.GetMethod("OnGUI", BindingFlags.NonPublic | BindingFlags.Instance);
+        //    methodInfo.Invoke(null, null);
+        //}
+    }
+}
--- Assets/Unity Exposer/Editor/PreviewGUI.cs
+++ Assets/Unity Exposer/Editor/PreviewGUI.cs
@@ -6,19 +6,37 @@
 	
 	public class PreviewGUI
 	{		
-		public static Type ActualType { get; private set; }
+		private static Type ActualType { get; set; }
 
 		static PreviewGUI()
 		{
+            //Type previewGUIType = typeof(UnityEditor.Editor).Assembly.GetType("PreviewGUI", true);
+            //Type previewGUIType = Type.GetType("PreviewGUI, UnityEditor", true);
+            //Debug.Log(previewGUIType.AssemblyQualifiedName);
+
 			ActualType = Type.GetType("PreviewGUI, UnityEditor", true);
 		}
+
+        public static void BeginScrollView(Rect position, Vector2 scrollPosition, Rect viewRect, GUIStyle horizontalScrollbar, GUIStyle verticalScrollbar)
+        {
+            MethodInfo methodInfo = ActualType.GetMethod("BeginScrollView", BindingFlags.NonPublic | BindingFlags.Static);
+            methodInfo.Invoke(null, new object[] { position, scrollPosition, viewRect, horizontalScrollbar, verticalScrollbar });
+        }
+
+        public static int CycleButton(int selected, GUIContent[] options)
+        {
+            MethodInfo methodInfo = ActualType.GetMethod("CycleButton", BindingFlags.Public | BindingFlags.Static);
+            return (int)methodInfo.Invoke(null, new object[] { selected, options });
+        }
+
+        public static Vector2 EndScrollView()
+        {
+            MethodInfo methodInfo = ActualType.GetMethod("EndScrollView", BindingFlags.Public | BindingFlags.Static);
+            return (Vector2)methodInfo.Invoke(null, null);
+        }
 		
 		public static Vector2 Drag2D(Vector2 scrollPosition, Rect position)
 		{
-			//Type previewGUIType = typeof(UnityEditor.Editor).Assembly.GetType("PreviewGUI", true);
-			//Type previewGUIType = Type.GetType("PreviewGUI, UnityEditor", true);
-			//Debug.Log(previewGUIType.AssemblyQualifiedName);
-
 			MethodInfo methodInfo = ActualType.GetMethod("Drag2D", BindingFlags.Public | BindingFlags.Static);
 			return (Vector2)methodInfo.Invoke(null, new object[] {scrollPosition, position});
 		}
