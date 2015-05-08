2ad8c9774cde1264ea91c02fa15c6c85f39df292
GlitchEnzo <mccarthp@gmail.com>
2015-05-08 16:43:29 -0700

    Added Preview example
    It doesn't use much in terms of internal features, but it is a starting
    point that can be beefed up later.

--- Assets/Unity Exposer/Tests/Editor/ExposerTestEditor.cs
+++ Assets/Unity Exposer/Tests/Editor/ExposerTestEditor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace UnityExposer.Tests
@@ -12,8 +13,37 @@ namespace UnityExposer.Tests
     {
         private List<GameObject> gameObjects;
 
+        private PreviewRenderUtility preview;
+
+        private Mesh sphere;
+
+        private Vector2 previewDirection = new Vector2();
+
+        private Material material;
+
+        static Mesh GetPreviewSphere()
+        {
+            GameObject gameObject = (GameObject)EditorGUIUtility.LoadRequired("Previews/PreviewMaterials.fbx");
+
+            gameObject.SetActive(false);
+            foreach (Transform transform in gameObject.transform)
+            {
+                if (transform.name == "sphere")
+                    return transform.GetComponent<MeshFilter>().sharedMesh;
+            }
+            return null;
+        }
+
         public void OnEnable()
         {
+            preview = new PreviewRenderUtility();
+            preview.m_CameraFieldOfView = 30f;
+
+            sphere = GetPreviewSphere();
+
+            //material = new Material(Shader.Find("Mobile/Diffuse"));
+            material = EditorGUIUtility.LoadRequired("Previews/Preview3DTextureMaterial.mat") as Material;
+
             gameObjects = new List<GameObject>();
             foreach (var unityObject in targets)
             {
@@ -26,6 +56,47 @@ namespace UnityExposer.Tests
             DrawDefaultInspector();
 
             AddComponentButton();
+
+            //preview.DrawMesh(sphere, Matrix4x4.identity, new Material(Shader.Find("Mobile/Diffuse")), 0);
+            OnPreviewGUI(GUILayoutUtility.GetRect(500, 500), GUIStyle.none);
+        }
+
+        public override void OnPreviewGUI(Rect r, GUIStyle background)
+        {
+            //Debug.Log("OnPreviewGUI");
+
+            //preview.BeginPreview(r, background);
+            //Bounds bounds = sphere.bounds;
+            //float magnitude = bounds.extents.magnitude;
+            //float num = 4f * magnitude;
+            //preview.m_Camera.transform.position = -Vector3.forward * num;
+            //preview.m_Camera.transform.rotation = Quaternion.identity;
+            //preview.m_Camera.nearClipPlane = num - magnitude * 1.1f;
+            //preview.m_Camera.farClipPlane = num + magnitude * 1.1f;
+            //preview.m_Light[0].intensity = 1.4f;
+            //preview.m_Light[0].transform.rotation = Quaternion.Euler(40f, 40f, 0.0f);
+            //preview.m_Light[1].intensity = 1.4f;
+            //Color ambient = new Color(0.1f, 0.1f, 0.1f, 0.0f);
+            //InternalEditorUtility.SetCustomLighting(preview.m_Light, ambient);
+            ////ModelInspector.RenderMeshPreviewSkipCameraAndLighting(mesh, bounds, previewUtility, litMaterial, wireMaterial, (MaterialPropertyBlock)null, direction, meshSubset);
+            //InternalEditorUtility.RemoveCustomLighting();
+            //Texture image = preview.EndPreview();
+            //GUI.DrawTexture(r, image, ScaleMode.StretchToFill, false);
+
+            previewDirection = PreviewGUI.Drag2D(previewDirection, r);
+            if (Event.current.type != EventType.Repaint)
+                return;
+            material.mainTexture = this.target as Texture;
+            preview.BeginPreview(r, background);
+            bool fog = RenderSettings.fog;
+            Unsupported.SetRenderSettingsUseFogNoDirty(false);
+            preview.m_Camera.transform.position = -Vector3.forward * 3f;
+            preview.m_Camera.transform.rotation = Quaternion.identity;
+            preview.DrawMesh(sphere, Vector3.zero, Quaternion.Euler(previewDirection.y, 0.0f, 0.0f) * Quaternion.Euler(0.0f, previewDirection.x, 0.0f), material, 0);
+            preview.m_Camera.Render();
+            Unsupported.SetRenderSettingsUseFogNoDirty(fog);
+            Texture image = preview.EndPreview();
+            GUI.DrawTexture(r, image, ScaleMode.StretchToFill, false);
         }
 
         private void AddComponentButton()
